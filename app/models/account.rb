e003e40e22a9d803e98c2261bdfc3c07f7470053
botto54 <bryanlbottorff@gmail.com>
2021-01-26 02:13:50 -0500

    add validates inclusion of transaction types in model to use in new update balance method in account model, add if else logic to update balance pending transaction type

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,19 @@
 class Account < ApplicationRecord
     has_many :transactions
-    
+
+    def update_balance(transaction)
+        if transaction.t_type == 'deposit' || 'credit'
+            self.balance = self.balance + transaction.t_amount
+            self.save
+        elsif transaction.t_type == 'debit' || 'withdrawal'
+            if self.balance >= transaction.t_amount
+                self.balance = self.balance - transaction.t_amount
+                self.save
+            else
+                return 'Balance too low.. =('
+            end
+        end
+    end
+
+
 end
