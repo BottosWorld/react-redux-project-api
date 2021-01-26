aee469adbe15e268c833035850c587b47eb3eb20
botto54 <bryanlbottorff@gmail.com>
2021-01-25 19:07:34 -0500

    build out transactions and accounts controller, add associations hasmany and belongsto in models, may need to add associations to db, next step is to write code to add/subtract from account balances when transactions are added

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,56 @@
 class TransactionsController < ApplicationController
-end
+    before_action :set_transaction, only: [:show, :update, :destroy]
+  
+    # GET /transactions
+    def index
+      if params[:account_id]
+        @account = Account.find(params[:account_id])
+        @transactions = @account.transactions
+      else
+        @transactions = Transaction.all
+      end
+  
+      render json: @transactions
+    end
+  
+    # GET /transactions/1
+    def show
+      render json: @transaction
+    end
+  
+    # POST /transactions
+    def create
+      @transaction = Transaction.new(transaction_params)
+  
+      if @transaction.save
+        render json: @transaction, status: :created, location: @transaction
+      else
+        render json: @transaction.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /transactions/1
+    def update
+      if @transaction.update(transaction_params)
+        render json: @transaction
+      else
+        render json: @transaction.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /transactions/1
+    def destroy
+      @transaction.destroy
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_transaction
+        @transaction = Transaction.find(params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def transaction_params
+        params.require(:transaction).permit(:t_name, :description, :t_type, :t_amount, :account_id)
+      end
+  end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,4 @@
 class Transaction < ApplicationRecord
+    belongs_to :account
+    validates :t_amount, presence: true
 end
